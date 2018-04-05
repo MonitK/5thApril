ed396784a61f96cad576344df211a2fe859cf452
MonitK <mkapoor@ddn.upes.ac.in>
2018-04-05 11:56:08 +0530

    Committing skelton code

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+void add(int,int);//Blaram is assigned
+void substract(int,int);//Darsh is assigned
+int main()
+{
+	int a,b,
+	    Cin>>a>>b;
+	add(a,b);
+	substract(a,b);
+}
